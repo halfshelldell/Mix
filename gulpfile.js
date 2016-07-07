978fa2f8e6fc69a1c816e30c883ac954e5c526db
geomaas <geoff.maas@gmail.com>
2016-07-07 16:48:46 -0400

    setup most of the structure ie templates controllers and services

---
+++ gulpfile.js
@@ -0,0 +1,38 @@
+'use strict'
+
+let gulp = require('gulp');
+let sass = require('gulp-sass');
+let browserify = require('gulp-browserify');
+
+gulp.task('default', ['html', 'css', 'js']);
+
+gulp.task('html', function () {
+  gulp.src('./templates/*.html').pipe(gulp.dest('./public/templates'));
+
+    return gulp.src('./index.html')
+        .pipe(gulp.dest('./public'));
+});
+
+gulp.task('css', function () {
+    return gulp.src('./styles.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('./public'));
+});
+
+gulp.task('js', function () {
+  // gulp.src('./controllers/*.js').pipe(gulp.dest('./public/controllers'));
+
+    return gulp.src('./js/app.js')
+        .pipe(browserify())
+        .pipe(gulp.dest('./public/js'))
+});
+
+gulp.task('watch', function () {
+    gulp.watch('./*.js', ['js']);
+    gulp.watch('./*/*.js', ['js']);
+    gulp.watch('./*.scss', ['css']);
+    gulp.watch('./index.html', ['html']);
+    gulp.watch('./templates/*.html', ['html']);
+    // gulp.watch('./controllers/*.js,'['js']);
+
+});
