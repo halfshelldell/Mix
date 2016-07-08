356cbafc9d28afd1b1ec03d2d56fc72a4e079b2d
michaeldelli-gatti <michaelddg@gmail.com>
2016-07-08 15:50:24 -0400

    add edit and delete

--- src/main/java/com/theironyard/services/RecipeRepository.java
+++ src/main/java/com/theironyard/services/RecipeRepository.java
@@ -9,4 +9,5 @@ import org.springframework.data.repository.CrudRepository;
  */
 public interface RecipeRepository extends CrudRepository<Recipe, Integer> {
     Iterable<Recipe> findByUser(User user);
+    Recipe findByRecipeName(String name);
 }
