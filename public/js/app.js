5c1032fb3bb788cbaa49f87ae70a1ab283f15881
geomaas <geoff.maas@gmail.com>
2016-07-09 14:54:07 -0400

    getting login functioning

--- public/js/app.js
+++ public/js/app.js
@@ -1,26 +1,39 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);throw new Error("Cannot find module '"+o+"'")}var f=n[o]={exports:{}};t[o][0].call(f.exports,function(e){var n=t[o][1][e];return s(n?n:e)},f,f.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 module.exports = function(mix) {
-    mix.controller('LoginController', ['$scope', 'LoginService', '$location', function($scope, LoginService, $location) {
+    mix.controller('CreateController', ['$scope', 'CreateService', '$location', function($scope, CreateService, $location) {
+
+        $scope.createRecipe = function() {
+            console.log("clicked create");
+
+        };
+
+
+    }]);
+};
+
+},{}],2:[function(require,module,exports){
+module.exports = function(mix) {
+    mix.controller('LoginController', ['$scope', 'LoginService', '$location', '$http', function($scope, LoginService, $location, $http) {
         $scope.username = "",
         $scope.userpassword = "",
 
         $scope.login = function() {
             console.log("clicked login");
+            $http({
+                  url: '/login',
+                  method: 'post',
+                  data: {
+                      username: $scope.username,
+                      password: $scope.userpassword,
+                  },
+              }).then(function () {
+                  $location.path('/mixmatch');
+              }).catch(function () {
+                  console.error('INTRUDER');
+                  $location.path('/shit')
+              });
 
         };
-        // $http({
-              // url: '/users',
-        //       method: 'post',
-        //       data: {
-        //           username: $scope.username,
-        //           password: $scope.password,
-        //       },
-        //   }).then(function () {
-        //       $location.path('/mixmatch');
-        //   }).catch(function () {
-        //       console.error('INTRUDER');
-        //       $location.path('/shit')
-        //   });
 
     }]);
 };
@@ -28,15 +41,16 @@ module.exports = function(mix) {
 // usera
 // passworda
 
-},{}],2:[function(require,module,exports){
+},{}],3:[function(require,module,exports){
 let mix = angular.module('mixApp', ['ngRoute']);
 //
 // // Controllers
 require('./controllers/logincontroller')(mix);
-// require('./js/controllers/questioncontroller')((mix;
+require('./controllers/createcontroller')(mix);
 //
 // // Services
 require('./services/loginservice')(mix);
+require('./services/createservice')(mix);
 
 
 mix.config(['$routeProvider', function ($routeProvider) {
@@ -65,7 +79,40 @@ mix.config(['$routeProvider', function ($routeProvider) {
         });
 }]);
 
-},{"./controllers/logincontroller":1,"./services/loginservice":3}],3:[function(require,module,exports){
+},{"./controllers/createcontroller":1,"./controllers/logincontroller":2,"./services/createservice":4,"./services/loginservice":5}],4:[function(require,module,exports){
+module.exports = function (mix) {
+    mix.factory('CreateService', ['$http', function ($http) {
+        let newrecipe = [];
+
+        return {
+          // $http({
+                // url: '/recipe????',
+          //       method: 'post',
+                // data: {
+                //     id:
+                //     recipeName:
+                //     instructions:
+                //     ingredients:
+                //     skill:
+                //     fileName:
+                //     user:
+                // },
+                // dataType: 'json',
+                //     contentType: "application/json"
+          //   });
+          
+                // return user;
+            // },
+        };
+    }]);
+};
+
+
+
+
+// Recipe Object…. int id, String recipeName, int time, String instructions, String ingredients, String skill, String fileName, User user
+
+},{}],5:[function(require,module,exports){
 module.exports = function (mix) {
     mix.factory('LoginService', ['$http', function ($http) {
         let user = [];
@@ -92,4 +139,4 @@ module.exports = function (mix) {
     }]);
 };
 
-},{}]},{},[2])
+},{}]},{},[3])
--- public/js/controllers/createcontroller.js
+++ public/js/controllers/createcontroller.js
@@ -1,24 +1,11 @@
 module.exports = function(mix) {
-    mix.controller('CreateController', ['$scope', 'LoginService', '$location', function($scope, LoginService, $location) {
-        $scope.username = "",
+    mix.controller('CreateController', ['$scope', 'CreateService', '$location', function($scope, CreateService, $location) {
 
-        $scope.login = function() {
-            console.log("clicked login");
+        $scope.createRecipe = function() {
+            console.log("clicked create");
 
         };
-        // $http({
-              // url: '/users',
-        //       method: 'post',
-        //       data: {
-        //           username: $scope.username,
-        //           password: $scope.password,
-        //       },
-        //   }).then(function () {
-        //       $location.path('/mixmatch');
-        //   }).catch(function () {
-        //       console.error('INTRUDER');
-        //       $location.path('/shit')
-        //   });
+
 
     }]);
 };
